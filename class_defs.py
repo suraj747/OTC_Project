2cc6225320b466f589dd6b66b6094e725f75d483
suraj747 <s.gosai90@gmail.com>
2013-12-04 17:26:36 +0000

    commented some code

--- class_defs.py
+++ class_defs.py
@@ -1,18 +1,31 @@
 class environment:
     def __init__(totalTime,timeStep,nDealers,nNoiseCustomers,NoiseCustomerFrequencies,nInfCustomers,InformedCustomerValues,connectivity):
+        #initialise system to start at time = 0
         self.current_t=0
+        #how long the total simulation will be (in minutes)
         self.T=totalTime
+        #size of time step....possibly consider making this very small so that
+        #I can put in a time penalty for certain actions
         selt.dt=timeStep
+        #create list of dealers (currently, unconfigured)
         self.dealers=[dealer() for each in range(nDealers)]
+        #create noise customers (currently have no set frequency, infored = 0)
         self.noiseCustomers=[noiseCustomer() for each in range(nNoiseCustomers)]
+        #create informed customers (currently all are set to default level of informed 1)
         self.infCustomers=[infCustomer() for each in range(nInfCustomers)]
+        #configure noise customers frequencies
         for i in range(nNoiseCustomers):
             self.noiseCustomers[i].setFreq(NoiseCustomerFrequencies[i])
+        #configure informed customer lvels
         for i in range(nInfCustomers):
             self.infCustomers[i].setInformedValue(InformedCustomerValues[i])
 
-    #def next_t(self):
-    #    self.current_t=self.current_t+self.dt
+    def next_t(self):
+        #run through processes
+        #first customers
+        #then dealers
+        
+        self.current_t=self.current_t+self.dt #go to next time step
 
 
 class customer:
